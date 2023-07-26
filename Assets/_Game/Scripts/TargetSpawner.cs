a80e3ee203b113686de610241f3ee7323c6a165c
Paris <paris@octr.dev>
2023-07-26 17:55:37 +0100

    Add Difficulty Scaling & Target Kill Tracking + Rocket Tag

--- Assets/_Game/Scripts/TargetSpawner.cs
+++ Assets/_Game/Scripts/TargetSpawner.cs
@@ -2,24 +2,42 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class TargetSpawner : MonoBehaviour
+public class TargetSpawner : Singleton<TargetSpawner>
 {
     [SerializeField] private GameObject[] targetPrefabs;
     [SerializeField] private Transform[] spawnPositions;
     [SerializeField] private bool isSpawned;
     [SerializeField] private float lifeTime = 5;
 
+    public int targetKillCount;
+
     private int cachedSpawn;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        targetKillCount = 0;
     }
 
     // Update is called once per frame
     void Update()
     {
+        if(lifeTime == 5)
+        {
+            if(targetKillCount == 10)
+            {
+                lifeTime = 3f;
+            }
+        }
+
+        if(lifeTime == 3)
+        {
+            if(targetKillCount == 20)
+            {
+                lifeTime = 2f;
+            }
+        }
+
         if(!isSpawned)
         {
             int spawnSeed = Random.Range(0, spawnPositions.Length);
